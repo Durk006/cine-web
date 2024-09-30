dfb532334c0241cd8cccf58e022a33b1e4535ffe
Jeffrey-Rivera <jeffreyrivera711@gmail.com>
2024-09-30 00:19:58 -0600

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
-import image from 'next/image'
-import Navbar from './Navbar'
+"use client";
+import { UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import Home from "./pages/home";
 
-export default function Home () {
-  return <Navbar/>;
+export default function title_page() {
+  const { isSignedIn } = useUser();
+
+  return (
+    <div>
+      {isSignedIn && <Home/>}
+      <div>
+        {!isSignedIn ? (
+          <div>
+            <Link href="/sign-up">Signup</Link>
+            <Link href="/sign-in">Login</Link>
+          </div>
+        ) : (
+          <UserButton afterSignOutUrl="/" />
+        )}
+      </div>
+    </div>
+  );
 }
