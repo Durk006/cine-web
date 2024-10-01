bb7d175682ee34ced16c61a65fea4eeb14de5388
JoshuaDalida <joshuamdalida@gmail.com>
2024-10-01 13:54:02 -0600

    upload form shows watch history
    it ONLY shows on the watch history page

--- src/app/components/api/uploadForm.jsx
+++ src/app/components/api/uploadForm.jsx
@@ -1,17 +1,10 @@
 'use client'
-import cloudinary from '../../cloudinary';
+
 const UploadForm = () => {
 
 
   const handleSubmit = async (e) => {
-    const results = await cloudinary.uploader.upload(e,
-      {
-          resource_type:"auto"
-      }
-  )
-  console.log(results)
-  const url = cloudinary.url(results.public_id)
-  console.log(url)
+    
   };
 
   return (
